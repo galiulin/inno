63c3487e300797a23f303bd2e12165f472a23fc1
art_inno <galiulin.arthur@gmail.com>
2017-12-07 21:41:33 +0300

    node realization

--- src/main/java/day_04/MyLinkedHashMap.java
+++ src/main/java/day_04/MyLinkedHashMap.java
@@ -1,197 +1,63 @@
 package day_04;
 
-import java.time.temporal.ValueRange;
-import java.util.Collection;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.LinkedList;
+import java.util.List;
 
-public class MyLinkedHashMap<K, V> implements Map {
+public class MyLinkedHashMap<K, V> {
 
+    private Node<K, V>[] hashTable;
+    private int size = 0;
+    private float threshold;
 
-    @Override
-    public Object getOrDefault(Object key, Object defaultValue) {
-        return null;
-    }
-
-    @Override
-    public void forEach(BiConsumer action) {
+    public void add(K key, V value) {
+        Node<K, V> newNode = new Node<>(key, value);
 
     }
 
-    @Override
-    public void replaceAll(BiFunction function) {
-
+    private int hash(final K key) {
+        int hash = 31;
+        hash = hash * 17 + key.hashCode();
+        return hash % hashTable.length;
     }
 
-    @Override
-    public Object putIfAbsent(Object key, Object value) {
-        return null;
-    }
-
-    @Override
-    public boolean remove(Object key, Object value) {
-        return false;
-    }
-
-    @Override
-    public boolean replace(Object key, Object oldValue, Object newValue) {
-        return false;
-    }
-
-    @Override
-    public Object replace(Object key, Object value) {
-        return null;
-    }
-
-    @Override
-    public Object computeIfAbsent(Object key, Function mappingFunction) {
-        return null;
-    }
-
-    @Override
-    public Object computeIfPresent(Object key, BiFunction remappingFunction) {
-        return null;
-    }
-
-    @Override
-    public Object compute(Object key, BiFunction remappingFunction) {
-        return null;
-    }
-
-    @Override
-    public Object merge(Object key, Object value, BiFunction remappingFunction) {
-        return null;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return false;
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return false;
-    }
-
-    @Override
-    public Object get(Object key) {
-        return null;
-    }
-
-    @Override
-    public Object put(Object key, Object value) {
-        return null;
-    }
-
-    @Override
-    public Object remove(Object key) {
-        return null;
-    }
-
-    @Override
-    public void putAll(Map m) {
-
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public Set keySet() {
-        return null;
-    }
-
-    @Override
-    public Collection values() {
-        return null;
-    }
-
-    @Override
-    public Set<Entry> entrySet() {
-        return null;
-    }
-
-    protected static class EntrySet<K, V>{
-
-        EntrySet<K, V> cursor;
-
+    private class Node<K, V> {
+        private List<Node<K, V>> nodes;
+        private int hash;
         private K key;
         private V value;
 
-        EntrySet<K, V> next;
-        EntrySet<K, V> previos;
-
-        private EntrySet(K key, V value){
+        private Node(K key, V value) {
             this.key = key;
             this.value = value;
-            cursor = this;
+            nodes = new LinkedList<Node<K, V>>();
         }
 
-        public void addEntry(K key, V value){
-            next = new EntrySet<>(key, value);
-            next.setPrevios(this);
+        private List<Node<K, V>> getNodes() {
+            return nodes;
         }
 
-        public boolean isHasNext(){
-            return next != null;
+        private int hash() {
+            return hashCode() % hashTable.length;
         }
 
-        public boolean isHasPrevios(){
-            return previos != null;
-        }
-
-        public EntrySet<K, V> next(){
-            if (!isHasNext()) throw new NoSuchElementException();
-            return next;
-        }
-
-        public EntrySet<K, V> previos() {
-            if (!isHasPrevios()) throw new NoSuchElementException();
-            return previos;
-        }
-
-        public K getKey(){
+        private K getKey() {
             return key;
         }
 
-        public V getValue(){
+        private V getValue() {
             return value;
         }
 
-        private void setPrevios(EntrySet<K, V> previos){
-            this.previos = previos;
+        private void setValue(V value) {
+            this.value = value;
         }
 
-        protected void deleteElement() {
-            if (isHasNext()) this.next.previos = this.previos;
-            if (isHasPrevios()) this.previos.next = this.next;
-        }
-    }
-
-    public static void main(String[] args) {
-        EntrySet<Integer, String> entrySet = new EntrySet<>(1, "one");
-        entrySet.addEntry(2, "two");
-        entrySet.addEntry(3, "three");
-        entrySet.addEntry(4, "four");
-        entrySet.addEntry(5, "five");
-        while (entrySet.isHasNext()){
-            System.out.println(entrySet.next);
+        @Override
+        public int hashCode() {
+            hash = 31;
+            hash = hash * 17 + key.hashCode();
+            hash = hash * 17 + value.hashCode();
+            return hash;
         }
     }
 }
