6a20992f7770ad9666db29c33091e99a60de3c56
art_inno <galiulin.arthur@gmail.com>
2017-12-08 00:20:13 +0300

    added myLinkedHashMap realization

--- src/main/java/day_04/MyLinkedHashMap.java
+++ src/main/java/day_04/MyLinkedHashMap.java
@@ -2,12 +2,121 @@ package day_04;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class MyLinkedHashMap<K, V> {
 
     private Node<K, V>[] hashTable;
     private int size = 0;
     private float threshold;
+
+    public MyLinkedHashMap() {
+        hashTable = new Node[16];
+        threshold = hashTable.length * 0.75f;
+    }
+
+    public boolean insert(final K key, final V value) {
+        if (size + 1 >= threshold) {
+            threshold *= 2;
+            arrayDoubling();
+        }
+
+        Node<K, V> newNode = new Node<>(key, value);
+        int index = hash(key);
+
+        if (hashTable[index] == null) {
+            return simpleAdd(index, newNode);
+        }
+
+        List<Node<K, V>> nodeList = hashTable[index].getNodes();
+
+        for (Node<K, V> node : nodeList) {
+            if (keyExistButValueNew(node, newNode, value) || collisionProceessing(node, newNode, nodeList)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean simpleAdd(int index, Node<K, V> newNode) {
+        hashTable[index] = new Node<>(null, null);
+        hashTable[index].getNodes().add(newNode);
+        size++;
+        return true;
+    }
+
+    private boolean keyExistButValueNew(
+            final Node<K, V> nodeFromList,
+            final Node<K, V> newNode,
+            final V value) {
+
+        if (newNode.getKey().equals(nodeFromList.getKey()) && !newNode.getValue().equals(nodeFromList.getValue())) {
+            nodeFromList.setValue(value);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean collisionProceessing(final Node<K, V> nodeFromList,
+                                         final Node<K, V> newNode,
+                                         final List<Node<K, V>> nodes) {
+        if (newNode.hashCode() == nodeFromList.hashCode() &&
+                !Objects.equals(newNode.key, nodeFromList.key) &&
+                !Objects.equals(newNode.value, nodeFromList.key)) {
+            nodes.add(newNode);
+            size++;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void arrayDoubling() {
+        Node<K, V>[] oldHashTable = hashTable;
+        hashTable = new Node[oldHashTable.length * 2];
+        size = 0;
+        for (Node<K, V> node : oldHashTable) {
+            if (node != null) {
+                for (Node<K, V> n : node.getNodes()) {
+                    insert(n.key, n.value);
+                }
+            }
+        }
+    }
+
+    public boolean delete(final K key) {
+        int index = hash(key);
+        if (hashTable[index] == null) return false;
+
+        if (hashTable[index].getNodes().size() == 1) {
+            hashTable[index] = null;
+            return true;
+        }
+
+        List<Node<K, V>> nodeList = hashTable[index].getNodes();
+        for (Node<K, V> node : nodeList) {
+            if (key.equals(node.getKey())) {
+                nodeList.remove(node);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public V get(final K key) {
+        int index = hash(key);
+        if (index < hashTable.length && hashTable[index] != null) { /*todo в будущем вынести проверки в отдельные функции
+                                                                        и при достижении лимитов выбрасывать исключения*/
+
+            List<Node<K, V>> list = hashTable[index].getNodes();
+            for (Node<K, V> node : list) {
+                if (key.equals(node.getKey())) {
+                    return node.getValue();
+                }
+            }
+        }
+        return null;
+    }
 
     public void add(K key, V value) {
         Node<K, V> newNode = new Node<>(key, value);
