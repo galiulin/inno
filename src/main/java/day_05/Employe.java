85e7e9ca5269364146adb832fcc0493b2c9f755a
Arthur <galiulin.arthur@gmail.com>
2017-12-08 12:13:12 +0300

    added employe realization

--- src/main/java/day_05/Employe.java
+++ src/main/java/day_05/Employe.java
@@ -1,6 +1,9 @@
 package day_05;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
+
 /*Итерация один сериализация по умолчанию
 * Итерация два дописать в конец файла сумму всех зарплат
 * Итерация три пользовательская сериализация
@@ -9,11 +12,77 @@ import java.util.List;
 *
 * решение должно быть грамотно разбито на классы, структурировано, работа с ошибками
 * подумать над тем что возможно стоит держать стрим открытым*/
-public class Employe {
+public class Employe implements Serializable {
     private String name;
-    private String age;
-    private String salary;
+    private int age;
+    private int salary;
     private String job;
+
+    public Employe(String name, int age, int salary, String job) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employe employe = (Employe) o;
+        return getAge() == employe.getAge() &&
+                getSalary() == employe.getSalary() &&
+                Objects.equals(getName(), employe.getName()) &&
+                Objects.equals(getJob(), employe.getJob());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getName(), getAge(), getSalary(), getJob());
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", job='" + job + '\'' +
+                '}';
+    }
 }
 
 class EmployeManager {
