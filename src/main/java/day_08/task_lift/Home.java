eac2d7ecafbedf0e6b3964799a5718f89362b46b
Arthur <galiulin.arthur@gmail.com>
2017-12-14 10:19:01 +0300

    updated addHuman

--- src/main/java/day_08/task_lift/Home.java
+++ src/main/java/day_08/task_lift/Home.java
@@ -6,21 +6,28 @@ public class Home {
     private final ConcurrentLinkedQueue<Pair> isWanted;
     private final int countFloors;
     private final Lift lift;
+    private final Thread threadWithLift;
 
     public Home(int countFloors) {
         this.isWanted = new ConcurrentLinkedQueue<>();
         this.countFloors = countFloors;
         this.lift = new Lift(isWanted);
 
-        Thread threadWithLift = new Thread(() -> lift.on());
+        threadWithLift = new Thread(() -> lift.on());
         threadWithLift.start();
     }
 
     public void addHuman(Human human, int userChoise) {
         Pair pair = new Pair(human, userChoise);
         isWanted.add(pair);
-        Thread thread = new Thread(()-> {synchronized (lift){lift.notifyAll();}});
-        thread.start();
+        System.out.println(threadWithLift.getState());
+        if (threadWithLift.getState() == Thread.State.WAITING) {
+            synchronized (lift) {
+                lift.notifyAll();
+            }
+//        Thread thread = new Thread(()-> {synchronized (lift){lift.notifyAll();}});
+//        thread.start();
+        }
 //        synchronized (lift) {
 //            lift.notifyAll();
 //        }
