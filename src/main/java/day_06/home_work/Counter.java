1018fb301323175aa5e9025b063ab7deae13d8ff
Arthur <galiulin.arthur@gmail.com>
2017-12-14 18:33:40 +0300

    edited counter, updated -now uses char array

--- src/main/java/day_06/home_work/Counter.java
+++ src/main/java/day_06/home_work/Counter.java
@@ -1,6 +1,8 @@
 package day_06.home_work;
 
 import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Counter {
     private int count;
@@ -58,19 +60,25 @@ public class Counter {
 
         File[] files = TestFiles.getFilesInFolder("src/main/java/trash/resources_for_tests/");
 
+        ExecutorService service = Executors.newFixedThreadPool(20);
+
+//        for (File file : files){
+//            service.submit(new MyCounterWords(counter, file, word));
+//        }
+
         Thread[] threads = new Thread[files.length];
         MyCounterWords[] myCounterWords = new MyCounterWords[files.length];
-
+//
         for (int i = 0; i < files.length; i++) {
             myCounterWords[i] = new MyCounterWords(counter, files[i], word);
             threads[i] = new Thread(myCounterWords[i]);
             threads[i].start();
         }
-
+//
         for (int i = 0; i < threads.length; i++) {
             threads[i].join();
             System.out.println(myCounterWords[i].getFile().getName() + " : " + myCounterWords[i].getLocalCount());
-//            System.out.println(i);
+            System.out.println(i);
         }
 
         counter.writeIntoFile();
@@ -92,7 +100,7 @@ class MyCounterWords implements Runnable {
 
     private final File file;
     private final Counter counter;
-    private final String word;
+    private final char[] word;
     private int localCount;
 
     @Override
@@ -107,7 +115,7 @@ class MyCounterWords implements Runnable {
     public MyCounterWords(Counter counter, File file, String word) {
         this.file = file;
         this.counter = counter;
-        this.word = word;
+        this.word = word.toLowerCase().toCharArray();
         this.localCount = 0;
     }
 
@@ -115,15 +123,15 @@ class MyCounterWords implements Runnable {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
             do {
                 String line = reader.readLine();
-                substringAndCheck(line.toLowerCase(), word.toLowerCase());
+                substringAndCheck(line.toLowerCase(), word);
             } while (reader.ready());
         }
     }
 
-    private void substringAndCheck(String review, String word) {
+    private void substringAndCheck(String review, char[] word) {
         String[] s = review.split(" ");
         for (int i = 0; i < s.length; i++) {
-            if (s[i].length() < word.length()) continue;
+            if (s[i].length() < word.length) continue;
             else if (checkLine(s[i], word) > 0) {
                 incrementCounterAndCheck();
             }
@@ -134,37 +142,40 @@ class MyCounterWords implements Runnable {
         localCount++;
         synchronized (counter) {
             counter.increment();
-            if (counter.getCount() % 5 == 0){
+            if (counter.getCount() % 5 == 0) {
                 counter.writeIntoFile();
             }
         }
     }
 
-    public static int checkLine(String review, String word) {
+    public static int checkLine(String review, char[] word) {
         char[] arrReview = review.toCharArray();
-        char[] arrVerify = word.toCharArray();
         int localCount = 0;
         int j = 0;
-        if (arrReview.length < arrVerify.length) return 0;
+//        if (arrReview.length < word.length) return 0; /*если длина проверяемого слова меньше длины слова то выходим*/ //временно убрал т.к. есть проверка
+                                                                        //в предыдущем уровне стека вызова данной функции и добавил проверку по оставшейся части
         for (int i = 0; i < arrReview.length; i++) {
-            if (arrReview[i] == arrVerify[j]) {
+            if (arrReview[i] == word[j]) {
                 j++;
-                if (j == arrVerify.length) {
+                if (j == word.length) {
                     localCount++;
                     j = 0;
                 }
             } else {
                 j = 0;
+                if (arrReview.length - i < word.length)
+                    return 0; /*если длина оставшейся части массива меньше чем длина слова то выходим
+                     todo посчитать среднюю длину слова в текстах для того что-бы понять получаем ли мы преимущество с этой проверкой*/
             }
         }
         return localCount;
     }
 
-    public File getFile(){
+    public File getFile() {
         return file;
     }
 
-    public int getLocalCount(){
+    public int getLocalCount() {
         return localCount;
     }
 }
