382eccac1a7c355e34a10a340a573150de47f036
art_inno <galiulin.arthur@gmail.com>
2017-12-16 13:52:30 +0300

    tempo

--- src/main/java/day_06/home_work/Counter.java
+++ src/main/java/day_06/home_work/Counter.java
@@ -62,29 +62,41 @@ public class Counter {
 
         ExecutorService service = Executors.newFixedThreadPool(20);
 
+//        String line = "страдание раз, страдание два, страдание три \r\n страдание четыре, пятое страдание";
+//        MyCounterWords.containString(line, "страдание");
+
+//        MyCounterWords[] myCounterWords = new MyCounterWords[files.length];
+//
 //        for (File file : files){
+//            int i = 0;
+//            myCounterWords[i] = new MyCounterWords(counter, file, word);
+//            service.submit(myCounterWords[i]);
+//            i++;
 //            service.submit(new MyCounterWords(counter, file, word));
 //        }
 
+        service.shutdown();
+
         Thread[] threads = new Thread[files.length];
         MyCounterWords[] myCounterWords = new MyCounterWords[files.length];
-//
+
         for (int i = 0; i < files.length; i++) {
             myCounterWords[i] = new MyCounterWords(counter, files[i], word);
             threads[i] = new Thread(myCounterWords[i]);
             threads[i].start();
         }
 //
-        for (int i = 0; i < threads.length; i++) {
-            threads[i].join();
+        Thread.sleep(10000);
+        for (int i = 0; i < myCounterWords.length; i++) {
+//            threads[i].join();
             System.out.println(myCounterWords[i].getFile().getName() + " : " + myCounterWords[i].getLocalCount());
             System.out.println(i);
         }
-
-        counter.writeIntoFile();
-        counter.closeWriter();
-
-        System.out.println(counter.getCount());
+//
+//        counter.writeIntoFile();
+//        counter.closeWriter();
+//
+//        System.out.println(counter.getCount());
     }
 }
 
@@ -122,10 +134,26 @@ class MyCounterWords implements Runnable {
     public void containString(File file) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
             do {
-                String line = reader.readLine();
-                substringAndCheck(line.toLowerCase(), word);
+                String line = reader.readLine().toLowerCase();
+                int poz = -1;
+                if (line.contains(word.toString())) {
+                    System.out.println(true);
+                }
+                while ((poz = line.indexOf(word.toString(), poz + 1)) != -1) {
+                    incrementCounterAndCheck();
+                }
+//                substringAndCheck(line.toLowerCase(), word);
             } while (reader.ready());
         }
+    }
+
+    public static int containString(String line, String word) {
+        int poz = -1;
+        int localCount = 0;
+        while ((poz = line.indexOf(word, poz + 1)) != -1) {
+            localCount++;
+        }
+        return localCount;
     }
 
     private void substringAndCheck(String review, char[] word) {
@@ -153,7 +181,7 @@ class MyCounterWords implements Runnable {
         int localCount = 0;
         int j = 0;
 //        if (arrReview.length < word.length) return 0; /*если длина проверяемого слова меньше длины слова то выходим*/ //временно убрал т.к. есть проверка
-                                                                        //в предыдущем уровне стека вызова данной функции и добавил проверку по оставшейся части
+        //в предыдущем уровне стека вызова данной функции и добавил проверку по оставшейся части
         for (int i = 0; i < arrReview.length; i++) {
             if (arrReview[i] == word[j]) {
                 j++;
