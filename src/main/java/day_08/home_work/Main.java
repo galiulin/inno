50edabcf2b4077f6e6514fe2b8ca8b571c5976d2
Arthur <galiulin.arthur@gmail.com>
2017-12-16 15:19:28 +0300

    mv src/main/java/day_08/home_work/Suffering.java/main() >> ./Main.java/mainExample()

--- src/main/java/day_08/home_work/Main.java
+++ src/main/java/day_08/home_work/Main.java
@@ -5,9 +5,7 @@ import Utils.TestFiles;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,10 +16,74 @@ public class Main {
                 Executors.newFixedThreadPool(20);
 
         List<Future<FileInfo>> futures = new ArrayList<>();
-        for (File file : files){
+        for (File file : files) {
             Future<FileInfo> future = service.submit(new Suffering<FileInfo>(file, "страдание"));
             futures.add(future);
         }
 
+    }
+
+
+    private void mainExample() {
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(15);
+        List<Future<Object>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            Future<Object> future =
+                    executorService.submit(new Callable<Object>() {
+                        @Override
+                        public Object call() {
+                            int result = 0;
+                            for (int i = 0; i < 100; i++) {
+                                try {
+                                    Thread.sleep(Thread.currentThread().getId() * 10);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                result += i;
+                            }
+                            return result;
+                        }
+                    });
+            futures.add(future);
+        }
+
+        while (true) {
+            List<Future<Object>> tempFutures =
+                    new ArrayList<>();
+            tempFutures.addAll(0, futures);
+
+            for (Future<Object> future :
+                    tempFutures) {
+                if (future.isDone()) {
+                    try {
+                        System.out.println(future.get());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    futures.remove(future);
+                }
+            }
+
+            if (futures.size() == 0)
+                return;
+        }
+
+        /*Future<Object> future = futures.get(0);
+
+        try {
+            System.out.println(
+                    future.get(5, TimeUnit.SECONDS)
+            );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }*/
     }
 }
