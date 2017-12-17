171865d6f971177822a36885adbd137b23e71845
Arthur <galiulin.arthur@gmail.com>
2017-12-17 20:20:19 +0300

    Suffering.java >> переделана на работу с очередью

--- src/main/java/day_09/home_work/Suffering.java
+++ src/main/java/day_09/home_work/Suffering.java
@@ -1,6 +1,7 @@
 package day_09.home_work;
 
 import java.io.*;
+import java.util.Queue;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
@@ -15,13 +16,15 @@ public class Suffering<T extends FileInfo> implements Callable {
     private static final Lock lock = new ReentrantLock();
     private static final Condition condition = lock.newCondition();
 
+    private static final BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(50, true);
+
     public final static AtomicInteger atomicInteger = new AtomicInteger();
 
     private static void incAtomInt() {
         lock.lock();
         try {
-            atomicInteger.incrementAndGet();
-            condition.signal();
+            queue.add(atomicInteger.incrementAndGet());
+//            condition.signal();
         } finally {
             lock.unlock();
         }
@@ -29,6 +32,20 @@ public class Suffering<T extends FileInfo> implements Callable {
 
     public static int getInt() {
         int i = 0;
+        if (true) {
+            try {
+                if (!Main.service.isTerminated()) {
+                    i = queue.poll(3, TimeUnit.SECONDS);
+                } else {
+                    i = atomicInteger.get();
+                }
+            } catch (NullPointerException ex) {
+                /*do nothing*/
+            } catch (InterruptedException e) {
+                /* do nothing */
+            }
+        }
+    /*
         try {
             lock.lock();
             if (!Main.service.isTerminated()) {
@@ -40,6 +57,7 @@ public class Suffering<T extends FileInfo> implements Callable {
         } finally {
             lock.unlock();
         }
+         */
         return i;
     }
 
