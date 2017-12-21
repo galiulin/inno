eaac4b828172529f8d65528bdc1268a1c0bb7c88
Arthur <galiulin.arthur@gmail.com>
2017-12-21 09:23:29 +0300

    в PoolConnections коллекция заменена на LinkedHashMap

--- src/main/java/day_13/home_work/PoolConnections.java
+++ src/main/java/day_13/home_work/PoolConnections.java
@@ -3,12 +3,12 @@ package day_13.home_work;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class PoolConnections {
-    private Map<URL, HttpURLConnection> map = new ConcurrentHashMap<>();
     private int maxSize = 50;
+    private Map<URL, HttpURLConnection> map = new LinkedHashMap<>(maxSize, 1.1f, true);
 
     public PoolConnections (int maxSize){
         this.maxSize = maxSize;
